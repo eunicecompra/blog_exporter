ae4714c4ad381e0d8a86f49cf89095111c09226e
Breinn Aguilar <breinn.aguilar@butterfly.com.au>
2020-10-18 22:47:34 +1100

    checked element is displayed before clicking

--- main.py
+++ main.py
@@ -5,15 +5,8 @@ driver = webdriver.Remote("http://localhost:4444/wd/hub", DesiredCapabilities.CH
 driver.get("http://inanywhereelse.blogspot.com/")
 assert driver.title.rfind("In Anywhere Else") != -1
 
-# elements = driver.find_elements_by_xpath("//a[contains(@href, '/2010/05/')]")
-# /assert len(elements) > 0
 elements = driver.find_elements_by_class_name("toggle")
-print(f"Number of elements {len(elements)}")
+assert len(elements) > 0
 for element in elements:
-    print(f"Tag name: " + element.tag_name)
-    if "a" == element.tag_name:
-        print("waiting for 5 seconds")
-        driver.implicitly_wait(5)
+    if element.is_displayed():
         element.click()
-
-driver.close()
