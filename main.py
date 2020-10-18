ffc562b84815d3b19e4fd4b82179a8a4eb8e1cc3
eunicecompra <eunicecompra@gmail.com>
2020-10-18 17:21:29 +1100

    iterating elements by toogle with error in first toggle

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 
-driver = webdriver.Remote("http://selenium:4444/wd/hub", DesiredCapabilities.CHROME)
+driver = webdriver.Remote("http://localhost:4444/wd/hub", DesiredCapabilities.CHROME)
 driver.get("http://inanywhereelse.blogspot.com/")
-print("taking screenshot...")
-driver.save_screenshot("/app/output/blog.png")
+assert driver.title.rfind("In Anywhere Else") != -1
+
+# elements = driver.find_elements_by_xpath("//a[contains(@href, '/2010/05/')]")
+# /assert len(elements) > 0
+elements = driver.find_elements_by_class_name("toggle")
+print(f"Number of elements {len(elements)}")
+for element in elements:
+    print(f"Tag name: " + element.tag_name)
+    if "a" == element.tag_name:
+        print("waiting for 5 seconds")
+        driver.implicitly_wait(5)
+        element.click()
+
 driver.close()
