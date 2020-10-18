a4c92bba99c22b3b095c78340073e3767ea0f4ae
Breinn Aguilar <breinn.aguilar@butterfly.com.au>
2020-10-18 23:13:47 +1100

    finding the blog post links

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ assert len(elements) > 0
 for element in elements:
     if element.is_displayed():
         element.click()
+
+elements = driver.find_elements_by_class_name("posts")
+assert len(elements) > 0
+for element in elements:
+    li_elements = element.find_elements_by_tag_name("li")
+    for li in li_elements:
+        blog_link = li.find_element_by_tag_name("a")
+        if blog_link:
+            print(blog_link.text)
